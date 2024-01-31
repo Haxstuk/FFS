f6741adf1a947c6572bf32cd800ef4218dc04cd2
mhh.hosseini <soldier.mh.2001@gmail.com>
2024-01-31 18:54:30 +0330

    changing ialloc to put files in the same parent dir's bg

--- fs.c
+++ fs.c
@@ -298,14 +298,16 @@ static struct inode* iget(uint dev, uint inum);
 // Allocate an inode on device dev.
 // Mark it as allocated by  giving it type type.
 // Returns an unlocked but allocated and referenced inode.
+// If the given type is of T_FILE, we use given pinum to try and allocate
+// an inode int he same block group as the parent directory.
 struct inode*
-ialloc(uint dev, short type)
+ialloc(uint dev, short type, uint pinum)
 {
   int inum;
   struct buf *bp;
   struct dinode *dip;
 
-    int until = sb.ninodes;
+  int until = sb.ninodes;
   inum = 1;
 
   if (type == T_DIR) {
@@ -314,6 +316,28 @@ ialloc(uint dev, short type)
     until = inum + sb.inodesperbgroup;
     if (inum == 0)
       inum = 1; // no inode 0
+     } else if (type == T_FILE) {
+    // try to find an inode in the same block group as pinum.
+    int blockgroup = IBLOCKGROUP(pinum, sb);
+    int start = FIRSTINODEOFBGROUP(blockgroup, sb);
+    int end = start + sb.inodesperbgroup;
+    if (start == 0)
+      start = 1; // no inode 0
+    int i;
+    for (i = start; i < end; i++) {
+      bp = bread(dev, IBLOCK(i, sb));
+      dip = (struct dinode*)bp->data + i%IPB;
+      if(dip->type == 0){  // a free inode
+        memset(dip, 0, sizeof(*dip));
+        dip->type = type;
+        log_write(bp);   // mark it allocated on the disk
+        brelse(bp);
+        return iget(dev, i);
+      }
+      brelse(bp);
+    }
+
+    // if we reach here, fall down to search all block groups for a free inode.
   }
   for(; inum < until; inum++){
     bp = bread(dev, IBLOCK(inum, sb));
